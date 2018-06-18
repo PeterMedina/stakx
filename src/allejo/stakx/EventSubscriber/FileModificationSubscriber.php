080124e11fb911ff0e8e36731f374f46322f75c0
Vladimir Jimenez <allejo@me.com>
2018-06-18 00:37:28 -0700

    Add start of new file watching process

--- src/allejo/stakx/EventSubscriber/FileModificationSubscriber.php
+++ src/allejo/stakx/EventSubscriber/FileModificationSubscriber.php
@@ -9,17 +9,20 @@ namespace allejo\stakx\EventSubscriber;
 
 use allejo\stakx\Compiler;
 use allejo\stakx\Core\StakxLogger;
+use allejo\stakx\FileMapper;
 use allejo\stakx\Filesystem\FilesystemLoader as fs;
 use Kwf\FileWatcher\Event\Modify as ModifyEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class FileModificationSubscriber implements EventSubscriberInterface
 {
+    private $fileMapper;
     private $compiler;
     private $logger;
 
-    public function __construct(Compiler $compiler, StakxLogger $logger)
+    public function __construct(Compiler $compiler, FileMapper $fileMapper, StakxLogger $logger)
     {
+        $this->fileMapper = $fileMapper;
         $this->compiler = $compiler;
         $this->logger = $logger;
     }
@@ -28,6 +31,35 @@ class FileModificationSubscriber implements EventSubscriberInterface
     {
         $relFilePath = fs::getRelativePath($event->filename);
         $this->logger->writeln(sprintf('File change detected: %s', $relFilePath));
+
+        switch ($this->fileMapper->getFileType($relFilePath))
+        {
+            case FileMapper::STATIC_PAGEVIEW:
+            case FileMapper::DYNAMIC_PAGEVIEW:
+            case FileMapper::REPEATER_PAGEVIEW:
+            {
+                $this->compiler->runtimeCompilePageViewFromPath($relFilePath);
+            }
+            break;
+
+            case FileMapper::DATA_ITEM:
+            case FileMapper::CONTENT_ITEM:
+            {
+                $this->compiler->runtimeCompileCollectableItemFromPath($relFilePath);
+            }
+            break;
+
+            case FileMapper::TWIG_INCLUDE:
+            {
+                $dependents = $this->fileMapper->getTemplateDependents($relFilePath);
+
+                foreach ($dependents as $dependent)
+                {
+                    $this->compiler->runtimeCompilePageViewFromPath($dependent);
+                }
+            }
+            break;
+        }
     }
 
     public static function getSubscribedEvents()
